cdde041842d0ef39891127fcd49efe6c3c99f0d6
Nikhil Nawgiri <the@realest.ninja>
2023-05-01 12:04:32 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 export default {
 	async fetch(request, env, ctx) {
+    const gatherResponse = async (response) => await response.json();
+
+    const sendMessageToTelegramUser = async ({ token, chatId, text }) => {
+      const telegramSendToUserUrl = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`
+      const telegramResponse = await fetch(telegramSendToUserUrl);
+      const responseData = await gatherResponse(telegramResponse);
+      console.log(JSON.stringify(responseData, null, 4));
+    };
+
     if (request.method === "POST") {
       const payload = await request.json()
       if ("message" in payload) {
         const { BOT_TOKEN } = env;
         const chatId = payload.message.chat.id
-        const response = `${payload.message.text} to you too`;
-
-        const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${response}`
-
-        console.log("url:", url);
-
-        const data = await fetch(url).then(resp => resp.json())
-        console.log("data:", data);
+        const text = `${payload.message.text} to you too`;
+        await sendMessageToTelegramUser({ token: BOT_TOKEN, chatId, text });
       }
     }
     return new Response("hello world");
