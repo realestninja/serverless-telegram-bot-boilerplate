7eb2ea8191e52d45f052287cf2d80f48b063230c
Nikhil Nawgiri <the@realest.ninja>
2023-07-02 19:55:52 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
+import { sendMessageToTelegramUser } from "./telegram";
+
 export default {
   async fetch(request, env, ctx) {
-    const gatherResponse = async (response) => await response.json();
-
-    const sendMessageToTelegramUser = async ({ token, chatId, text }) => {
-      const telegramSendToUserUrl = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`
-      const telegramResponse = await fetch(telegramSendToUserUrl);
-      const responseData = await gatherResponse(telegramResponse);
-      console.log(JSON.stringify(responseData, null, 4));
-    };
-
     if (request.method === "POST") {
       const payload = await request.json()
       if ("message" in payload) {
